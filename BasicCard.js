7387a4775d05c62a51ce4bf76c0f7e858718c764
wahula4 <mawahula4@gmail.com>
2017-07-30 11:27:03 -0600

    basic and cloze working

--- BasicCard.js
+++ BasicCard.js
@@ -1,55 +1,69 @@
 // dependency for inquirer npm package
 var inquirer = require("inquirer");
-// constructor function used to create question objects
-function BasicFlash(question, answer) {
+var basicQuestions = require("./log.json");
+var counter = 0;
+var correctAnswerCount = 0;
+
+function ClozeCard(question, answer){
   this.question = question;
   this.answer = answer;
-}
-// creates the printInfo method and applies it to all question objects
-BasicFlash.prototype.printInfo = function() {
-  console.log("Question: " + this.question + "\nAnswer: " + this.answer);
-  console.log("---------------");
 };
-// variable we will use to count how many times our questions have been asked
-var count = 0;
-// array in which we will store each of our new question objects
-var questionArray = [];
-var askQuestion = function() {
-  // if statement to ensure that our questions are only asked five times
-  if (count < 2) {
-    console.log("NEXT QUESTION");
-    // runs inquirer and asks the user a series of questions whose replies are
-    // stored within the variable answers inside of the .then statement
-    inquirer.prompt([
-      {
-        name: "q",
-        message: "What is the question?"
-      }, {
-        name: "a",
-        message: "What is the answer?"
-      }
-    ]).then(function(answers) {
-      // initializes the variable newQuestion to be a question object which will
-      // take in all of the user's answers to the questions above
-      var newQuestion = new BasicFlash(
-        answers.q,
-        answers.a);
-      // pushes newQuestion object into our array
-      questionArray.push(newQuestion);
-      // add one to count to increment our recursive loop by one
-      count++;
-      // run the askquestion function again so as to either end the loop or ask the questions again
-      askQuestion();
-    });
-    // else statement which runs a for loop that will execute .printInfo() for each object inside of our array
-  }
-  else {
-    for (var i = 0; i < questionArray.length; i++) {
-      questionArray[i].printInfo();
-    }
-  }
-};
-// call askQuestion to run our code
-askQuestion();
 
-// module.exports = BasicCard;
+var askQuestions = function (){
+
+  if(counter < 5){
+
+  inquirer.prompt([
+
+    {type: "input",
+      message: basicQuestions[counter].question,
+      name: "question"
+      }//if
+ ]).then(function(answer){
+
+  var userInput = answer.question.toLowerCase();
+
+    if(userInput === basicQuestions[counter].answer){
+          console.log("Correct!");
+          correctAnswerCount++;
+        }//if
+
+        else{
+          console.log("Wrong!");
+          console.log(basicQuestions[counter].answer);
+        }//else
+
+  counter++
+  askQuestions();
+
+  });//closing then
+} //closing if
+
+else{
+  console.log("Game Over!")
+  console.log("Correct Answers: " + correctAnswerCount);
+  inquirer.prompt([
+
+      {type: "confirm",
+        message: "Do you want to play again?",
+        name: "playAgain",
+        default: true
+        }
+    ]).then(function(answer){
+
+      if (answer.playAgain === true){
+        counter = 0;
+        correctAnswerCount = 0;
+        askQuestions();
+
+      }
+      else{
+        console.log("Thank you for playing!");
+      }
+
+  });
+}
+
+}; //closing function
+
+askQuestions();
--- ClozeCard.js
+++ ClozeCard.js
@@ -1,2 +1,80 @@
+var inquirer = require("inquirer");
+var clozeQuestions = require("./clozeQuestions.json");
+var counter = 0;
+var correctAnswerCount = 0;
 
-module.exports = ClozeCard;
+function ClozeCard(partial, cloze){
+  this.cloze = cloze;
+  this.partial = partial + "...";
+  this.fullText = partial + " " + cloze;
+  this.returnCloze = function(){
+    console.log("Answer: " + this.cloze);
+};
+  this.returnPartial = function(){
+  console.log(this.partial);
+};
+  this.returnFullText = function(){
+  console.log(this.fullText);
+
+};
+
+};
+
+var askQuestions = function (){
+
+  if(counter < 5){
+
+  inquirer.prompt([
+
+    {type: "input",
+      message: clozeQuestions[counter].partial + "...",
+      name: "question"
+      }//if
+ ]).then(function(answer){
+
+  var userInput = answer.question.toLowerCase();
+
+    if(userInput === clozeQuestions[counter].cloze){
+          console.log("Correct!");
+          correctAnswerCount++;
+        }//if
+
+        else{
+          console.log("Wrong!");
+          console.log(clozeQuestions[counter].fullAnswer);
+        }//else
+
+  counter++
+  askQuestions();
+
+  });//closing then
+} //closing if
+
+else{
+  console.log("Game Over!")
+  console.log("Correct Answers: " + correctAnswerCount);
+  inquirer.prompt([
+
+      {type: "confirm",
+        message: "Do you want to play again?",
+        name: "playAgain",
+        default: true
+        }
+    ]).then(function(answer){
+
+      if (answer.playAgain === true){
+        counter = 0;
+        correctAnswerCount = 0;
+        askQuestions();
+
+      }
+      else{
+        console.log("Thank you for playing!");
+      }
+
+  });
+}
+
+}; //closing function
+
+askQuestions();
